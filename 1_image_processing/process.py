7982e7be775844ac4c8acd093611a0cf154ff432
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-11-06 00:58:43 +0100

    image processing - wild try, contour evaluation

--- 1_image_processing/process.py
+++ 1_image_processing/process.py
@@ -32,34 +32,37 @@ def count_blood_cells(image_path):
         cv2.fillPoly(invadabingreenimg, pts=[contour], color=255)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     oinvadabingreenimg = cv2.morphologyEx(invadabingreenimg, cv2.MORPH_OPEN, kernel, iterations=2)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    coinvadabingreenimg = cv2.morphologyEx(oinvadabingreenimg, cv2.MORPH_CLOSE, kernel, iterations=2)
-    dtcoinvadabingreenimg = cv2.distanceTransform(coinvadabingreenimg, cv2.DIST_L2, 5)
-    _, temp = cv2.threshold(dtcoinvadabingreenimg, 0.7 * dtcoinvadabingreenimg.max(), 255, 0)
-    defwhitecells = np.uint8(temp)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    whitecells = cv2.dilate(defwhitecells, kernel, iterations=1)
-    _, whitecellscontours, _ = cv2.findContours(whitecells, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    _, whitecellscontours, _ = cv2.findContours(oinvadabingreenimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     white_blood_cell_count = len(whitecellscontours)
     adabingreenimg = cv2.adaptiveThreshold(greenimg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 535, 0)
     invadabingreenimg = 255 - adabingreenimg
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    surewhitecells = cv2.dilate(coinvadabingreenimg, kernel, iterations=3)
-    redinvadabingreenimg = cv2.subtract(invadabingreenimg, surewhitecells)
-    img, contours, hierarchy = cv2.findContours(redinvadabingreenimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    erodedinvadabingreenimg = cv2.morphologyEx(invadabingreenimg, cv2.MORPH_ERODE, kernel, iterations=2)
+    img, contours, hierarchy = cv2.findContours(erodedinvadabingreenimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-        cv2.fillPoly(redinvadabingreenimg, pts=[contour], color=255)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    erodedredinvadabingreenimg = cv2.erode(redinvadabingreenimg, kernel, iterations=4)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    dilatederodedredinvadabingreenimg = cv2.dilate(erodedredinvadabingreenimg, kernel, iterations=2)
-    dtdilatederodedredinvadabingreenimg = cv2.distanceTransform(dilatederodedredinvadabingreenimg, cv2.DIST_L2, 5)
-    _, temp = cv2.threshold(dtdilatederodedredinvadabingreenimg, 0.35 * dtdilatederodedredinvadabingreenimg.max(), 255, 0)
-    defredcells = np.uint8(temp)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    redcells = cv2.dilate(defredcells, kernel, iterations=1)
-    _, redcellscontours, _ = cv2.findContours(redcells, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    red_blood_cell_count = len(redcellscontours)
+        cv2.fillPoly(erodedinvadabingreenimg, pts=[contour], color=255)
+    _, contours, _ = cv2.findContours(erodedinvadabingreenimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    redcellcount = 0
+    for contour in contours:
+        cimg = np.zeros(shape=erodedinvadabingreenimg.shape, dtype=np.uint8)
+        cv2.fillPoly(cimg, pts=[contour], color=255)
+        whites = 0
+        for wcontour in whitecellscontours:
+            wimg = np.zeros(shape=erodedinvadabingreenimg.shape, dtype=np.uint8)
+            cv2.fillPoly(wimg, pts=[wcontour], color=255)
+            if cv2.countNonZero(cv2.bitwise_and(cimg, wimg)) != 0:
+                whites = whites + 1
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+                wimg = cv2.morphologyEx(wimg, cv2.MORPH_DILATE, kernel, iterations=2)
+                cimg = cv2.subtract(cimg, wimg)
+        if whites == 0:
+            redcellcount = redcellcount + 1
+        else:
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+            cimg = cv2.morphologyEx(cimg, cv2.MORPH_OPEN, kernel, iterations=3)
+            _, cimgcontours, _ = cv2.findContours(cimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            redcellcount = redcellcount + len(cimgcontours)
+    red_blood_cell_count = redcellcount
     has_leukemia = True if red_blood_cell_count/(red_blood_cell_count+white_blood_cell_count)<0.925 else False
     # TODO - Odrediti da li na osnovu broja krvnih zrnaca pacijent ima leukemiju i vratiti True/False kao povratnu vrednost ove procedure
     #if white_blood_cell_count<=2 and red_blood_cell_count/white_blood_cell_count>12:
