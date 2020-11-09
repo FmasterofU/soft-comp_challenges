cbab24ad590bc2e84458ea58a8501d20c04c1fb2
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-11-10 00:56:20 +0100

    image processing - last submission, corrected leukemia prediction

--- 1_image_processing/process.py
+++ 1_image_processing/process.py
@@ -19,7 +19,6 @@ def count_blood_cells(image_path):
     white_blood_cell_count = 0
     has_leukemia = None
 
-    # TODO - Prebrojati crvena i bela krvna zrnca i vratiti njihov broj kao povratnu vrednost ove procedure
     cvimg = cv2.imread(image_path)
     greenimg = cvimg[:, :, 1].astype('float64')
     greenimg *= (255.0 / greenimg.max())
@@ -35,9 +34,8 @@ def count_blood_cells(image_path):
     white_blood_cell_count = len(whitecellscontours)
     adabingreenimg = cv2.adaptiveThreshold(greenimg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 535, 0)
     invadabingreenimg = 255 - adabingreenimg
-    #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    #einvadabingreenimg = cv2.morphologyEx(invadabingreenimg, cv2.MORPH_ERODE, kernel, iterations=1)
-    einvadabingreenimg = invadabingreenimg
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    einvadabingreenimg = cv2.morphologyEx(invadabingreenimg, cv2.MORPH_ERODE, kernel, iterations=1)
     img, contours, hierarchy = cv2.findContours(einvadabingreenimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         cv2.fillPoly(einvadabingreenimg, pts=[contour], color=255)
@@ -45,8 +43,25 @@ def count_blood_cells(image_path):
     oeinvadabingreenimg = cv2.morphologyEx(einvadabingreenimg, cv2.MORPH_OPEN, kernel, iterations=3)
     _, cellscontours, _ = cv2.findContours(oeinvadabingreenimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     red_blood_cell_count = len(cellscontours) - white_blood_cell_count
-    has_leukemia = True if red_blood_cell_count/(red_blood_cell_count+white_blood_cell_count)<0.925 else False
-    # TODO - Odrediti da li na osnovu broja krvnih zrnaca pacijent ima leukemiju i vratiti True/False kao povratnu vrednost ove procedure
-    #if white_blood_cell_count<=2 and red_blood_cell_count/white_blood_cell_count>12:
-    #    has_leukemia = False
+    has_leukemia = True if red_blood_cell_count/(red_blood_cell_count+white_blood_cell_count) < 0.925 else False
+    if white_blood_cell_count > 1 and white_blood_cell_count <= 4:
+        m = cv2.moments(whitecellscontours[0])
+        cX = int(m["m10"] / m["m00"])
+        cY = int(m["m01"] / m["m00"])
+        center = [cX, cY]
+        canMergeCells = True
+        for c in whitecellscontours:
+            m = cv2.moments(c)
+            cX = int(m["m10"] / m["m00"])
+            cY = int(m["m01"] / m["m00"])
+            if abs(center[0] - cX) < 0.16 * greenimg.shape[1] and abs(center[1] - cY) < 0.16 * greenimg.shape[0]:
+                center = [(center[0] + cX) / 2, (center[1] + cY) / 2]
+            else:
+                canMergeCells = False
+                break
+        if canMergeCells:
+            white_blood_cell_count = 1
+            has_leukemia = False
+    has_leukemia = True if white_blood_cell_count > 4 and red_blood_cell_count <= 1800 else has_leukemia
+    has_leukemia = False if white_blood_cell_count == 1 else has_leukemia
     return red_blood_cell_count, white_blood_cell_count, has_leukemia
